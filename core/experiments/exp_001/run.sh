e45a15bd98cac50d04792d2a950d3fbe66673473
phos-x <phos_x@ip-52-44-1-126.us-west-2.compute.internal>
2026-03-12 16:08:39 +0000

    cleanup

--- core/experiments/exp_001/run.sh
+++
@@ -1,3 +0,0 @@
-﻿#!/usr/bin/env bash
-# Run experiment exp_001 via orchestrator
-python orchestrator/run_experiment.py --name exp_sepformer_mel
--- core/experiments/exp_003/run.sh
+++
@@ -1,3 +0,0 @@
-﻿#!/usr/bin/env bash
-# Run experiment exp_003 via orchestrator
-python orchestrator/run_experiment.py --name exp_sepformer_mel
